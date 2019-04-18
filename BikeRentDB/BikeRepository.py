b31ff899314b2ed08f4eb6dbcda2eb22483a131f
orzecheudezet <krzysztof.m.orzechowski@gmail.com>
2019-04-18 18:20:41 +0200

    Test

--- BikeRentDB/BikeRepository.py
+++ BikeRentDB/BikeRepository.py
@@ -1,10 +1,14 @@
+
+
 class BikeRepository:
     def __init__(self, cursor):
         self.Cursor = cursor
 
 
     def save(self, bike):
-        self.curosr.execute("INSERT INTO Bikes (Color, Size, Brand) VALUES(('"+bike.Color+"', '"+bike.Size+"', '"+bike.Brand+"'))")
+        self.Cursor.execute("INSERT INTO Bikes (Color, Size, Brand) VALUES('"+bike.Color+"', '"+bike.Size+"', '"+bike.Brand+"')")
+
+
 
 
 # ćwiczenia stożków rotatorów, 4 flex Faceless trójboista z Polski
